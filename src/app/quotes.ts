e4d3e7a10fa65c51d5c9e4d6d8528b93c2bede05
s-odhiambo <samuelangienda1998@gmail.com>
2019-10-29 10:33:18 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,8 +0,0 @@
-export class Quotes {
-  public showDescription: boolean;
-  QuotesUpVoted: any;
-  QuotesDownVoted: any;
-  constructor(public id: number,public name: string,public description: string, public author:string,public upvotes:number,public downvotes:number, public completeDate: Date){
-    this.showDescription=false;
-  }
-}
